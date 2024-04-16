5e1d3b3f9dde104875d0dc3543b6a2b61ce49efb
HyeseonLee <hyesun1999@gmail.com>
2024-04-16 21:55:21 +0900

    디버깅 제거

--- 1654.py
+++ 1654.py
@@ -16,13 +16,12 @@ def solution():
     while min_l <= max_l:
         # 중간 길이 계산
         mid = (min_l + max_l)//2
-        print(f"max {max_l} min {min_l} mid {mid}")
+
         # 몇개의 랜선이 생기는지 계산
         collected_line = 0
         for line in k_arr:
             collected_line += line // mid
-        
-        print("CL", collected_line)
+
         # 랜선 갯수의 총합이 >= N일 때, l = mid, 최대의 mid를 찾아야하기에 뒤에도 있는지 찾고 싶어서 min_l = mid+1
         if collected_line >= n:
             l = mid
@@ -35,5 +34,4 @@ def solution():
     return l
 
 if __name__ == "__main__":
-    print((2**31-1) // (195225786))
     print(solution())
