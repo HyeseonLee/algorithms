7214a664d9c7dc6be045e623aebcf47e4a1e72b6
HyeseonLee <hyesun1999@gmail.com>
2024-05-04 23:46:58 +0900

    boj 30

---
+++ 10610.py
@@ -0,0 +1,19 @@
+import sys
+
+    
+def solution():
+    res = 0
+    num = sys.stdin.readline().strip()
+    num_arr = [int(num) for num in num]
+    num_arr.sort(reverse=True) #O(5)
+
+    if 0 not in num_arr:
+        return -1
+    
+    if sum(num_arr)%3 == 0:
+        return ''.join(map(str, num_arr))
+    else:
+        return -1
+
+if __name__ == "__main__":
+    print(solution())
