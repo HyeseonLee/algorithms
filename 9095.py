e4a21e2fada5bdca42b1bb219fed7d1d2308bd1b
HyeseonLee <hyesun1999@gmail.com>
2024-05-21 02:29:02 +0900

    memo, tab 2가지 버전으로 작성

--- 9095.py
+++ 9095.py
@@ -2,31 +2,53 @@ import sys
 
 def solution():
     t = int(sys.stdin.readline().strip())
-    answer = []
+    answer_memo = []
+    answer_tab = []
 
     for _ in range(t):
         n = int(sys.stdin.readline().strip())
-        dp = [-1] * (n+1)
-        dp[0] = 1
+        dp_m = [-1] * (n+1)
+        # dp[0] = 1 # 이 문제에선 n이 양수이기에 이거 필요 없음
 
-        def sol(i):
-            if i<0:
+        def sol(n):
+            if n<0:
                 return 0
-            if i==0:
+            if n==0:
                 return 1
             
-            if dp[i] != -1:
-                return dp[i]
+            if dp_m[n] != -1:
+                return dp_m[n]
             
-            dp[i] = sol(i-1)+sol(i-2)+sol(i-3)
-            return dp[i]
+            dp_m[n] = sol(n-1)+sol(n-2)+sol(n-3)
+            return dp_m[n]
         
         ans = sol(n)
-        answer.append(ans)
+        answer_memo.append(ans)
 
-    for item in answer:
-        print(item)
+        ## 표 방식
+        dp_t = [0]*(n+1)
+        dp_t[0] = 1
+        def tab(n):
+            for i in range(1, n+1):
+                if i>=1:
+                    dp_t[i] += dp_t[i-1]
+                if i>=2:
+                    dp_t[i] += dp_t[i-2]
+                if i>=3:
+                    dp_t[i] += dp_t[i-3]
+            return dp_t[n]
+
+        ans2 = tab(n)
+        answer_tab.append(ans2)
     
+
+    # 결과 값 출력
+    for item in answer_memo:
+        print(item)
+
+    for item in answer_tab:
+        print(item)
+
     return None
 
 if __name__ == "__main__":
