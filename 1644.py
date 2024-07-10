2a10de83f11c9a86c675aab8432a447c63358670
HyeseonLee <hyesun1999@gmail.com>
2024-07-11 01:24:23 +0900

    1644

--- 1644.py
+++ 1644.py
@@ -3,7 +3,7 @@ import math
 
 n = int(sys.stdin.readline().strip())
 
-def find_prime_arr(n):
+def find_prime_arr(n): # O(10^6 log(log10^6))
     is_prime = [True] * (n+1)
     is_prime[0]=is_prime[1] = False
     for i in range(2, int(math.sqrt(n))+1):
@@ -22,20 +22,27 @@ def find_prime_sum_case(prime_arr, n):
     prime_sum = 0
     case = 0
 
-    while right <= len(prime_arr):
-        # print("prime_sum", prime_sum, "left", left, "right", right)
+    while right < len(prime_arr):
         if prime_sum == n:
             case += 1
             prime_sum -= prime_arr[left]
             left += 1
         elif prime_sum > n:
             prime_sum -= prime_arr[left]
+            left += 1
         else:
-            if right < len(prime_arr):
-                prime_sum += prime_arr[right]
+            prime_sum += prime_arr[right]
             right += 1
 
+    # prime_sum이 n과 같을 경우 체크
+    while prime_sum >= n and left < len(prime_arr):
+        if prime_sum == n:
+            case += 1
+        prime_sum -= prime_arr[left]
+        left += 1
+
     return case
+
 res = find_prime_sum_case(prime_arr, n)
 
 print(res)
