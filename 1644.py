ebdacf66b421b1016f7202c25942eac8080daff7
HyeseonLee <hyesun1999@gmail.com>
2024-07-11 01:08:30 +0900

    1644

---
+++ 1644.py
@@ -0,0 +1,41 @@
+import sys
+import math
+
+n = int(sys.stdin.readline().strip())
+
+def find_prime_arr(n):
+    is_prime = [True] * (n+1)
+    is_prime[0]=is_prime[1] = False
+    for i in range(2, int(math.sqrt(n))+1):
+        if is_prime[i]:
+            for j in range(2*i, n+1, i):
+                is_prime[j] = False
+    return [k for k in range(2,n+1) if is_prime[k]]
+
+prime_arr = find_prime_arr(n)
+
+# print(prime_arr)
+
+def find_prime_sum_case(prime_arr, n):
+    left = 0
+    right = 0
+    prime_sum = 0
+    case = 0
+
+    while right <= len(prime_arr):
+        # print("prime_sum", prime_sum, "left", left, "right", right)
+        if prime_sum == n:
+            case += 1
+            prime_sum -= prime_arr[left]
+            left += 1
+        elif prime_sum > n:
+            prime_sum -= prime_arr[left]
+        else:
+            if right < len(prime_arr):
+                prime_sum += prime_arr[right]
+            right += 1
+
+    return case
+res = find_prime_sum_case(prime_arr, n)
+
+print(res)
