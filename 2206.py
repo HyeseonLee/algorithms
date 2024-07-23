d96a52af2c08f3a674ca9810482b3ab2f28297a8
HyeseonLee <hyesun1999@gmail.com>
2024-07-24 01:22:17 +0900

    2206

--- 2206.py
+++ 2206.py
@@ -3,27 +3,31 @@ from collections import deque
 
 def bfs(n,m,graph):
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    visited = [[[False] * 2 for _ in range(m)] for _ in range(n)]
+    dist = [[[0] * 2 for _ in range(m)] for _ in range(n)]
     
-    queue = deque([(0, 0, 0)]) 
-    visited[0][0][0] = 1  
+    q = deque([(0, 0, 0)]) 
+    visited[0][0][0] = True
+    dist[0][0][0] = 1
     
-    while queue:
-        x, y, broken = queue.popleft()
+    while q:
+        x, y, broken = q.popleft()
         
         if x == n - 1 and y == m - 1:
-            return visited[x][y][broken]
+            return dist[x][y][broken]
         
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
             
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 0 and visited[nx][ny][broken] == 0: # 갈 수 있는 곳인 0인 경우
-                    visited[nx][ny][broken] = visited[x][y][broken] + 1
-                    queue.append((nx, ny, broken))
-                elif graph[nx][ny] == 1 and broken == 0 and visited[nx][ny][1] == 0: # 벽인 곳인 1인 경우에, broken 횟수가 1이고, 방문 가능할 경우
-                    visited[nx][ny][1] = visited[x][y][broken] + 1
-                    queue.append((nx, ny, 1))
+                if graph[nx][ny] == 0 and not visited[nx][ny][broken]: # 갈 수 있는 곳인 0인 경우
+                    visited[nx][ny][broken] = True
+                    dist[nx][ny][broken] = dist[x][y][broken] + 1
+                    q.append((nx, ny, broken))
+                elif graph[nx][ny] == 1 and not visited[nx][ny][1] and broken == 0: # 벽인 곳인 1인 경우에, broken 횟수가 0이고, 방문 가능할 경우
+                    visited[nx][ny][1] = True
+                    dist[nx][ny][1] = dist[x][y][broken] + 1
+                    q.append((nx, ny, 1))
     
     return -1
 
